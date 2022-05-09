22872340f4a5059aec9dcbfd694206592f39fc02
96olbap <alvinwanjala@gmail.com>
2022-05-10 02:50:12 +0300

    This commit will create the profile template.

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from flask import render_template,redirect,request,urll_for,abort
+from ..models import User
 from . import main
 
 # Views
@@ -9,3 +10,12 @@ def index():
     '''
 
     return render_template('index.html')
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template('profile/profile.html', user = user)
