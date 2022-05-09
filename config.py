f10ae31cd4992b69fea593bad8c7584788bb8877
96olbap <alvinwanjala@gmail.com>
2022-05-10 00:39:07 +0300

    set sqlalchemy_track_modifications to false

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config:
     General configuration parent class
     '''
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://apple:pass123@localhost/pitch'
-
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProdConfig(Config):
     '''
