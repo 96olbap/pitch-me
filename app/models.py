7e36e920214a0936b230b35f8747a364d1cbf5e2
96olbap <alvinwanjala@gmail.com>
2022-05-09 20:37:07 +0300

    This commit will initialize and create migrations in the db

--- app/models.py
+++ app/models.py
@@ -5,8 +5,25 @@ class User(db.Model):
     User class that will define user objects
     '''
     __tablename__ = 'users'
-    user_id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255), index = True)
+    # email = db.Column(db.String(255),unique= True, index = True)
+    # bio =db.Column(db.String(255))
+    # profile_pic_path = db.Column(db.String())
+    # pass_secure = db.Column(db.String(255))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
 
     def __repr__(self):
         return f'User { self.username }'
+
+class Role(db.Model):
+    '''
+    Role class that will define role objects
+    '''
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User', backref = 'role', lazy = 'dynamic')
+
+    def __repr__(self):
+        return f'User {self.name}'
