4ed7cc60d5431ca71b5e1e76c9b3657c15087dd7
96olbap <alvinwanjala@gmail.com>
2022-05-11 15:11:34 +0300

    This commit will update styling on landing page

--- app/__init__.py
+++ app/__init__.py
@@ -12,9 +12,13 @@ login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 photos = UploadSet('photos', IMAGES)
 
+# Initializing mail extension
+# mail = Mail()
+
 # Create app
 def create_app(config_name):
     app = Flask(__name__)
+
 
     # Creating app configurations
     app.config.from_object(config_options[config_name])
--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 export SECRET_KEY = 12345
-
+export MAIL_USERNAME=alvin.wanjala@student.moringaschool.com
+ecport MAIL_PASSWORD=S!norita8
 
 
 python3.10 manage.py server
